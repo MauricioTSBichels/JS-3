ac4245280a9e84a200de976c322419d281fa8761
Mauricio Telles Silva Bichels <143547107+MauricioTSBichels@users.noreply.github.com>
2025-03-05 12:44:25 -0300

    cpf.js

---
+++ cpf.js
@@ -0,0 +1,28 @@
+let cpfus=10532260988;
+let ver;
+digit11();
+
+function digit11(){
+ console.log(cpfus)   
+ let digitos=cpfus.toString().split('').length  ;
+ console.log(digitos)
+ if (digitos==11){
+ vercpf();
+ }
+}
+function vercpf (){   
+ let ver;
+ let ver2;
+ var n;
+ var n2;
+ let digt= cpfus.toString().split ();
+ console.log(digt)
+ while (n<11) {
+    n++
+    n--
+    ver=(digt[n])
+    ver2=ver.n2;
+ }
+ ver=(digt[n])
+
+}
